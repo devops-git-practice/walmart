f9a7c51f2d6c617bc3585f9d3405b268062760fc
Manir3025 <manir.m.9254@gmail.com>
2022-06-27 13:22:56 +0530

    adding add file

--- conf.sh
+++
@@ -1,4 +0,0 @@
-This  line is updated in master branch
-This line is updated in dev branch...
-This line is added for using option -a using purpose..
-This file is created for geting the merge conflict..
